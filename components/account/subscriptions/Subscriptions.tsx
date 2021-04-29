af889c32b74b3ef3f67a7412c9fcd92f1c5c6291
Kyle Hoskins <kyle2829@gmail.com>
2021-04-28 23:14:58 -0500

    feat: subscription card layout

--- components/account/subscriptions/Subscriptions.tsx
+++ components/account/subscriptions/Subscriptions.tsx
@@ -1,20 +1,42 @@
-import { Card, CardActionArea, CardContent } from '@material-ui/core';
+import { Card, CardActionArea, CardContent, CardActions, Button, Grid } from '@material-ui/core';
 import React from 'react';
 import { useGet } from 'utils/hooks/useGet';
+import { useSession } from 'next-auth/client';
 
 const Subscriptions = () => {
-  const { status, data: subscriptions } = useGet('/api/stripe/subscriptions', 'subscriptions');
-  //const data: any[] = [];
-  return (
-    <div>
-      {subscriptions?.map(subscription => (
-        <Card key={subscription.id}>
-          <CardActionArea>
-            <CardContent>{subscription.id}</CardContent>
-          </CardActionArea>
-        </Card>
-      ))}
-    </div>
+  const [session, loading] = useSession();
+  //const { status, data: subscriptions } = useGet('/api/stripe/subscriptions', 'subscriptions');
+  const subscriptions: any[] = [{ id: 'whatever' }, { id: 'another' }];
+  return (<>
+    <h2>Subscriptions</h2>
+    { subscriptions?.length > 0 ? (
+      <Grid container spacing={4}>
+        {subscriptions?.map(subscription => (
+          <Grid item xs="12" sm="6" md="3" key={subscription.id}>
+            <Card>
+              <CardActionArea>
+                <CardContent>{subscription.id}</CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button size="medium" color="primary">
+                  Manage
+            </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>) : <Card>
+      <CardActionArea>
+        <CardContent>No subscription found relating to the email associated with this account: {session?.user?.email}</CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="medium" color="primary">
+          Become a Member
+        </Button>
+      </CardActions>
+    </Card>
+    }
+  </>
   );
 };
 
