37c87ad99b0bcfa7cd4c1b5e34d5e6f1a9ff6eff
Kyle Hoskins <kyle2829@gmail.com>
2021-07-10 20:30:38 -0500

    feat: starting AddByName component

--- components/AddByName.tsx
+++ components/AddByName.tsx
@@ -1,3 +1,31 @@
-export default function AddByName() {
-  return <></>;
+import { Button, TextField } from '@material-ui/core';
+import { useState } from 'react';
+
+export default function AddByName({
+  label,
+  buttonText,
+  action,
+}: {
+  label: string;
+  buttonText: string;
+  action: (newName: string) => Promise<void>;
+}) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [newName, setNewName] = useState('');
+
+  async function add() {
+    console.log('adding');
+    setIsLoading(true);
+    await action('newName');
+    setIsLoading(false);
+    setNewName('');
+  }
+  return (
+    <>
+      <TextField label={label} value={newName} onChange={event => setNewName(event.target.value)}></TextField>
+      <Button disabled={!newName || isLoading} onClick={add}>
+        {buttonText}
+      </Button>
+    </>
+  );
 }
--- pages/admin/roles.tsx
+++ pages/admin/roles.tsx
@@ -12,13 +12,18 @@ const RolesPage = () => {
   const { data: roles, refetch: refetchRoles } = useGet<Role[]>('/api/roles', 'roles');
   const { data: users, refetch: refetchUsers } = useGet<Role[]>('/api/users', 'users');
 
+  async function addRole(roleName: string) {
+    console.log('roleName', roleName);
+    return;
+  }
+
   return (
     <Layout title='Roles'>
       <h1>Manage Roles</h1>
       <UserRoleTable roles={roles} users={users} refetch={refetchUsers}></UserRoleTable>
       <Divider></Divider>
       <RoleTable roles={roles} refetch={refetchRoles}></RoleTable>
-      <AddByName></AddByName>
+      <AddByName label='Add a Role' buttonText='Add Role' action={addRole}></AddByName>
     </Layout>
   );
 };
