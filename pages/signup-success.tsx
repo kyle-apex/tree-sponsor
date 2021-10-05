c7cea5f443b7b248694657b288ae1e84d8fa6bff
Kyle Hoskins <kyle@kylehoskins.com>
2021-10-05 07:08:03 -0500

    progress: signup-success

--- pages/signup-success.tsx
+++ pages/signup-success.tsx
@@ -1,20 +1,55 @@
-import { Button } from '@mui/material';
+import { Box, Button, Container } from '@mui/material';
+import Layout from 'components/layout/Layout';
 import { signIn } from 'next-auth/client';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Stripe, stripe } from 'utils/stripe/init';
+import Image from 'next/image';
 
-const SignupSuccess = ({ name }: { name: string }) => {
+const SignupSuccess = ({ name, email }: { name?: string; email?: string }) => {
+  useEffect(() => {
+    if (email) signIn('email', { email: email, redirect: false });
+  }, []);
   return (
-    <div>
-      {name}
-      <Button
-        onClick={() => {
-          signIn('email', { email: 'kyle@kylehoskins.com', redirect: false });
-        }}
-      >
-        Sign in
-      </Button>
-    </div>
+    <Layout>
+      <Container maxWidth='xs' sx={{ minHeight: 'calc(100vh - 170px)', marginBottom: 0, display: 'flex' }}>
+        <Box display='flex' justifyContent='center' alignItems='center'>
+          <Box
+            flexDirection='column'
+            display='flex'
+            justifyContent='center'
+            className='box-shadow'
+            sx={{
+              borderColor: theme => theme.palette.primary.main,
+              borderRadius: '5px',
+              border: 'solid 1px',
+              padding: '10px 20px 30px',
+              backgroundColor: 'white',
+              minHeight: '380px',
+            }}
+          >
+            <Box className='center' m={3}>
+              <Image src='/logo.png' alt='TreeFolks Young Professionals Logo' width={100} height={100} />
+            </Box>
+            {!email && (
+              <div className='center'>
+                <p>Error!</p>
+              </div>
+            )}
+            {email && (
+              <div className='center'>
+                <h2>Thanks for your donation!</h2>
+                <p>A login link to get started with your account was sent to your email address: {email || ''}</p>
+                {false && (
+                  <Button fullWidth variant='outlined' color='primary'>
+                    Retry Login
+                  </Button>
+                )}
+              </div>
+            )}
+          </Box>
+        </Box>
+      </Container>
+    </Layout>
   );
 };
 
@@ -23,7 +58,7 @@ export default SignupSuccess;
 export async function getServerSideProps(context: any) {
   const { req } = context;
 
-  const props: { name?: string } = {};
+  const props: { name?: string; email?: string } = {};
 
   try {
     const session = await stripe.checkout.sessions.retrieve(req.query.session_id);
@@ -31,6 +66,7 @@ export async function getServerSideProps(context: any) {
     const customer = (await stripe.customers.retrieve(session.customer as string)) as Stripe.Customer;
 
     props.name = customer.name;
+    props.email = customer.email;
     console.log('customer', customer);
   } catch (err: unknown) {
     props.name = null;
