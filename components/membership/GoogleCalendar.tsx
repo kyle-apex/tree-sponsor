d9bc4f9d0a603b7736bac35da4fe0e5910710b38
Kyle Hoskins <kyle@kylehoskins.com>
2021-11-17 22:57:52 -0600

    fix: style on Membership tab

--- components/membership/GoogleCalendar.tsx
+++ components/membership/GoogleCalendar.tsx
@@ -3,13 +3,14 @@ const GoogleCalendar = () => {
     <iframe
       src={
         process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_URL ||
-        'https://calendar.google.com/calendar/embed?src=treefolks.org_m6jreenfi823mfc8td4hva4i50%40group.calendar.google.com&ctz=America%2FChicago&mode=AGENDA'
+        'https://calendar.google.com/calendar/embed?src=treefolks.org_m6jreenfi823mfc8td4hva4i50%40group.calendar.google.com&ctz=America%2FChicago&mode=AGENDA&bgcolor=%2371998c&color=%230B8043'
       }
-      style={{ border: 0 }}
+      style={{ border: 'solid 5px #71998C' }}
       width='100%'
       height='400'
       frameBorder='0'
       scrolling='no'
+      className='box-shadow'
     ></iframe>
   );
 };
--- components/membership/MembershipPerks.tsx
+++ components/membership/MembershipPerks.tsx
@@ -4,26 +4,37 @@ import GoogleCalendar from './GoogleCalendar';
 const MembershipPerks = () => {
   return (
     <>
-      <Typography variant='h2'>Stay in Touch</Typography>
-      <a
-        href={
-          process.env.NEXT_PUBLIC_SLACK_INVITE_URL ||
-          'https://join.slack.com/t/treefolksyp/shared_invite/zt-y3mfodm8-8mix4P5~9OU9vPeqD7jrZA'
-        }
-        target='_blank'
-        rel='noreferrer'
-      >
-        Join Us on Slack
-      </a>
-      <br></br>
-      <a
-        href={process.env.FACEBOOK_GROUP_INVITE_URL || 'https://www.facebook.com/groups/2524568387782501'}
-        target='_blank'
-        rel='noreferrer'
-      >
-        Join The TreeFolks Young Professionals Facebook Group
-      </a>
-      <Typography variant='h2'>Upcoming Events</Typography>
+      <Typography variant='h2' color='secondary'>
+        Stay in Touch
+      </Typography>
+      <ul>
+        <li>
+          <a
+            href={
+              process.env.NEXT_PUBLIC_SLACK_INVITE_URL ||
+              'https://join.slack.com/t/treefolksyp/shared_invite/zt-y3mfodm8-8mix4P5~9OU9vPeqD7jrZA'
+            }
+            target='_blank'
+            rel='noreferrer'
+            style={{ textDecoration: 'none' }}
+          >
+            <Typography color='primary'>Join us on Slack</Typography>
+          </a>
+        </li>
+        <li>
+          <a
+            href={process.env.FACEBOOK_GROUP_INVITE_URL || 'https://www.facebook.com/groups/2524568387782501'}
+            target='_blank'
+            rel='noreferrer'
+            style={{ textDecoration: 'none' }}
+          >
+            <Typography color='primary'>Join the TreeFolks Young Professionals Facebook group</Typography>
+          </a>
+        </li>
+      </ul>
+      <Typography variant='h2' color='secondary'>
+        Upcoming Events
+      </Typography>
       <GoogleCalendar></GoogleCalendar>
     </>
   );
