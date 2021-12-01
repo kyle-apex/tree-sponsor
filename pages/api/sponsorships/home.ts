6cb72b5c8a943f5c8b7b369c696100c5a65e8ef7
Kyle Hoskins <kyle@kylehoskins.com>
2021-12-01 07:39:06 -0600

    feat: featured sponsorship ids for index page

--- pages/api/sponsorships/home.ts
+++ pages/api/sponsorships/home.ts
@@ -1,12 +1,30 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { prisma } from 'utils/prisma/init';
+import { Prisma } from '@prisma/client';
 
+//SponsorshipWhereInput
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'GET') {
+    const whereClause: Prisma.SponsorshipWhereInput = {
+      reviewStatus: { notIn: ['Draft', 'Rejected'] },
+      isPrivate: false,
+      isPrivateLocation: false,
+    };
+
+    if (process.env.FEATURED_SPONSORSHIP_IDS) {
+      const ids = process.env.FEATURED_SPONSORSHIP_IDS.trim()
+        .split(',')
+        .map(id => Number(id.trim()));
+
+      if (ids.length > 2) {
+        whereClause.id = { in: ids };
+      }
+    }
+
     const sponsorships = await prisma.sponsorship.findMany({
-      where: { reviewStatus: { notIn: ['Draft', 'Rejected'] } },
+      where: whereClause,
       include: { tree: {}, user: { select: { name: true, image: true, profilePath: true } } },
-      orderBy: { startDate: 'desc' },
+      orderBy: [{ description: 'desc' }, { startDate: 'desc' }],
       take: 3,
     });
     res.status(200).json(sponsorships);
