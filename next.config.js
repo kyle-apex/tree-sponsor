00ee9e9cb7b1ab44e6e98b55465582280c0c2bd4
Kyle Hoskins <kyle@kylehoskins.com>
2022-01-20 07:35:34 -0600

    disable swc minify

--- next.config.js
+++ next.config.js
@@ -3,5 +3,5 @@ module.exports = {
   images: {
     domains: [`${process.env.AWS_BUCKET_NAME}.s3.amazonaws.com`, 'platform-lookaside.fbsbx.com', 'lh3.googleusercontent.com'],
   },
-  swcMinify: true,
+  swcMinify: false,
 };
