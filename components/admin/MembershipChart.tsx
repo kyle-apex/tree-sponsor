2d0b5e3f014bb39125f805c8f103b2f5f9efe8d3
Kyle Hoskins <kyle@kylehoskins.com>
2022-03-28 07:51:54 -0500

    progress: stacked membership chart

---
+++ components/admin/MembershipChart.tsx
@@ -0,0 +1,105 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  BarControllerChartOptions,
+  CoreChartOptions,
+  DatasetChartOptions,
+  ElementChartOptions,
+  PluginChartOptions,
+  ScaleChartOptions,
+  ChartData,
+} from 'chart.js';
+import { _DeepPartialObject } from 'chart.js/types/utils';
+import { Bar } from 'react-chartjs-2';
+import useTheme from '@mui/system/useTheme';
+import { useState, useEffect } from 'react';
+import parsedGet from 'utils/api/parsed-get';
+
+type ChartDataResult = {
+  labels: string[];
+  active: number[];
+  inactive: number[];
+  total: number[];
+};
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+
+const options: _DeepPartialObject<
+  CoreChartOptions<'bar'> &
+    ElementChartOptions<'bar'> &
+    PluginChartOptions<'bar'> &
+    DatasetChartOptions<'bar'> &
+    ScaleChartOptions<'bar'> &
+    BarControllerChartOptions
+> = {
+  plugins: {
+    title: {
+      display: true,
+      text: 'Remaining Active Members Joining in a Given Year',
+    },
+  },
+  responsive: true,
+  interaction: {
+    mode: 'index' as const,
+    intersect: false,
+  },
+  scales: {
+    x: {
+      stacked: true,
+    },
+    y: {
+      stacked: true,
+    },
+  },
+};
+
+//options.indexAxis = 'y';
+
+const MembershipChart = () => {
+  const theme = useTheme();
+  const [chartData, setChartData] = useState<ChartData<'bar', number[], unknown>>(null);
+
+  const getData = async () => {
+    //const results = await parsedGet<ChartDataResult>('/api/members/active-by-year');
+    const results: ChartDataResult = {
+      labels: ['2019', '2020', '2021', '2022'],
+      total: [6, 12, 6, 8],
+      active: [2, 6, 5, 8],
+      inactive: [4, 6, 1, 0],
+    };
+    const data = {
+      labels: results.labels,
+      datasets: [
+        {
+          label: 'Active',
+          data: results.active,
+          backgroundColor: theme.palette.primary.light,
+        },
+        {
+          label: 'Inactive',
+          data: results.inactive,
+          backgroundColor: '#eda8a8',
+        },
+      ],
+    };
+    setChartData(data);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <>
+      {chartData && <Bar options={options} data={chartData} />}
+      {!chartData && <p>No data</p>}
+    </>
+  );
+};
+
+export default MembershipChart;
