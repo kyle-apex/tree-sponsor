27a9bd11134c5c4daec470ef241e6f99b9a72bc7
Kyle Hoskins <kyle@kylehoskins.com>
2022-03-30 20:49:45 -0500

    fix: prettier build issue

--- components/admin/MembershipStats.tsx
+++ components/admin/MembershipStats.tsx
@@ -10,7 +10,6 @@ import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import MembershipAttritionChart from './MembershipAttritionChart';
 import MembershipChart from './MembershipChart';
 
-
 const lastMonth = new Date();
 lastMonth.setDate(lastMonth.getDate() - 30);
 
