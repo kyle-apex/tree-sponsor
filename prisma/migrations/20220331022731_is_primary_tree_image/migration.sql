5a15482bd59133b56983c050d3492ca2f9889f14
Kyle Hoskins <kyle@kylehoskins.com>
2022-03-30 22:02:11 -0500

    fix: migration

--- prisma/migrations/20220331022731_is_primary_tree_image/migration.sql
+++ prisma/migrations/20220331022731_is_primary_tree_image/migration.sql
@@ -1,31 +1,3 @@
--- DropForeignKey
-ALTER TABLE
-    `Profile` DROP FOREIGN KEY `Profile_ibfk_1`;
-
--- DropForeignKey
-ALTER TABLE
-    `Sponsorship` DROP FOREIGN KEY `Sponsorship_ibfk_3`;
-
--- DropForeignKey
-ALTER TABLE
-    `Sponsorship` DROP FOREIGN KEY `Sponsorship_ibfk_2`;
-
--- DropForeignKey
-ALTER TABLE
-    `Sponsorship` DROP FOREIGN KEY `Sponsorship_ibfk_1`;
-
--- DropForeignKey
-ALTER TABLE
-    `Subscription` DROP FOREIGN KEY `Subscription_ibfk_2`;
-
--- DropForeignKey
-ALTER TABLE
-    `Subscription` DROP FOREIGN KEY `Subscription_ibfk_3`;
-
--- DropForeignKey
-ALTER TABLE
-    `TreeImage` DROP FOREIGN KEY `TreeImage_ibfk_1`;
-
 -- AlterTable
 ALTER TABLE
     `Category`
@@ -60,22 +32,6 @@ ADD
 
 -- AddForeignKey
 ALTER TABLE
-    `Subscription`
-ADD
-    CONSTRAINT `Subscription_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE
-SET
-    NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE
-    `Subscription`
-ADD
-    CONSTRAINT `Subscription_productId_fkey` FOREIGN KEY (`productId`) REFERENCES `Product`(`id`) ON DELETE
-SET
-    NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE
     `Category`
 ADD
     CONSTRAINT `Category_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE
@@ -103,37 +59,5 @@ ALTER TABLE
     `Event`
 ADD
     CONSTRAINT `Event_roleId_fkey` FOREIGN KEY (`roleId`) REFERENCES `Role`(`id`) ON DELETE
-SET
-    NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE
-    `TreeImage`
-ADD
-    CONSTRAINT `TreeImage_sponsorshipId_fkey` FOREIGN KEY (`sponsorshipId`) REFERENCES `Sponsorship`(`id`) ON DELETE
-SET
-    NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE
-    `Sponsorship`
-ADD
-    CONSTRAINT `Sponsorship_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE
-SET
-    NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE
-    `Sponsorship`
-ADD
-    CONSTRAINT `Sponsorship_subscriptionId_fkey` FOREIGN KEY (`subscriptionId`) REFERENCES `Subscription`(`id`) ON DELETE
-SET
-    NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE
-    `Sponsorship`
-ADD
-    CONSTRAINT `Sponsorship_treeId_fkey` FOREIGN KEY (`treeId`) REFERENCES `Tree`(`id`) ON DELETE
 SET
     NULL ON UPDATE CASCADE;
