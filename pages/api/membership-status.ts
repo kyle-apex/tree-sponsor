64af0ada7fd25d78857bcfc479afc336fa498ae2
Kyle Hoskins <kyle@kylehoskins.com>
2022-06-17 18:20:30 -0500

    fix: branch merge for verifying/updating accounts

--- pages/api/membership-status.ts
+++ pages/api/membership-status.ts
@@ -2,6 +2,7 @@ import { PinpointEmail } from 'aws-sdk';
 import { NextApiRequest, NextApiResponse } from 'next';
 import throwError from 'utils/api/throw-error';
 import { prisma } from 'utils/prisma/init';
+import { updateSubscriptionsForUser } from 'utils/stripe/update-subscriptions-for-user';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const email = req.query.email as string;
@@ -12,6 +13,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     where: { email: email },
     orderBy: { lastPaymentDate: 'desc' },
   });
+
+  await updateSubscriptionsForUser(email);
 
   console.log('subscriptionWithDetails', subscriptionWithDetails);
 
