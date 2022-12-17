be1e3340914c6f2d4fde0facbcc34be382627c93
Kyle Hoskins <kyle2829@gmail.com>
2022-12-17 17:21:47 -0600

    feat: simplified index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import Layout from 'components/layout/Layout';
+import Layout from '../components/layout/Layout';
 import { useEffect, useState } from 'react';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
@@ -20,6 +20,7 @@ import AtmosphereImage from 'components/index/icons/AtmosphereImage';
 import ActivitiesImage from 'components/index/icons/ActivitiesImage';
 import AnimalsImage from 'components/index/icons/AnimalsImage';
 import SponsorshipGroup from 'components/sponsor/SponsorshipGroup';
+import SignupForm from 'components/membership/SignupForm';
 
 const useStyles = makeStyles(theme => ({
   headlineContainer: {
@@ -40,7 +41,7 @@ const treeBenefits: TitleSection[] = [
   { title: 'Atmosphere', description: 'Think of your favorite picnic, restaurant, bar, and/or park enhanced by trees' },
   { title: 'Activities', description: 'Do not forget to be grateful for climbing, swinging, decorating, and much more!' },
 ];
-
+// Join us at our tree plantings, giveaways, tree maintenance, and cleanup events'
 const getIcon = (title: string) => {
   switch (title) {
     case 'Pollution':
@@ -57,7 +58,7 @@ const getIcon = (title: string) => {
       return <ActivitiesImage />;
   }
 };
-
+/*
 export const getStaticProps = async () => {
   return {
     props: {
@@ -65,8 +66,18 @@ export const getStaticProps = async () => {
     },
   };
 };
-
-const IndexPage = ({ treeBenefits }: { treeBenefits: TitleSection[] }) => {
+*/
+const IndexPage = ({
+  treeBenefits,
+  stripePriceIdLow,
+  stripePriceIdMedium,
+  stripePriceIdHigh,
+}: {
+  treeBenefits: TitleSection[];
+  stripePriceIdLow: string;
+  stripePriceIdMedium: string;
+  stripePriceIdHigh: string;
+}) => {
   const [sponsorships, setSponsorships] = useState<PartialSponsorship[]>([]);
   const [isLoadingSponsorships, setIsLoadingSponsorships] = useState(false);
 
@@ -86,56 +97,18 @@ const IndexPage = ({ treeBenefits }: { treeBenefits: TitleSection[] }) => {
   return (
     <Layout isFullWidth={true}>
       <Container maxWidth='lg'>
-        <Grid
-          mt={-4}
-          container
-          className={classes.headlineContainer + ' headline-section index'}
-          direction={{ xs: 'column', sm: 'row', md: 'row' }}
-          spacing={5}
-        >
-          <Grid sm={5} md={5} xs={12} justifyContent='center' sx={{ display: 'flex', flexDirection: 'column' }} item>
-            <Typography variant='h2' color='secondary'>
-              Thank-a-Tree with TreeFolksYP
-            </Typography>
-            <Typography variant='subtitle1' className={classes.headlineSubTitle}>
-              Thank your favorite trees with a Token of Appre-tree-ation in support of future tree plantings through TreeFolks Young
-              Professionals (TreeFolksYP)
-            </Typography>
-            <Grid container direction='row' spacing={2}>
-              <Grid item>
-                <Link href='/explore'>
-                  <Button variant='outlined' color='secondary'>
-                    Explore the Map
-                  </Button>
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href='/signup'>
-                  <Button variant='contained' color='primary'>
-                    Thank a Tree
-                  </Button>
-                </Link>
-              </Grid>
-            </Grid>
-          </Grid>
-          <Grid item md={7} sm={7} xs={12} direction='column' container alignContent='center' justifyContent='center'>
-            <Box
-              sx={{
-                height: '475px',
-                maxHeight: 'calc(75vh)',
-                width: '100%',
-                display: 'flex',
-                flexDirection: 'column',
-                /*backgroundImage: 'url(/pin-ring.svg)',
-                backgroundRepeat: 'no-repeat',
-                backgroundAttachment: 'fixed',
-                backgroundPosition: 'center center',*/
-              }}
-            >
-              <SponsorshipMap isIndexPage={true}></SponsorshipMap>
-            </Box>
-          </Grid>
-        </Grid>
+        <Box sx={{ marginTop: '0px !important' }} p={5} className='index detail-section'>
+          <Typography variant='h2'>What is TreeFolks Young Professionals?</Typography>
+          <Typography variant='subtitle1' className={classes.headlineSubTitle}>
+            TreeFolks Young Professionals (ages 21 – 40ish) volunteer, educate, fundraise, and build community in support of the mission of
+            TreeFolks: planting, caring for, and giving people free trees to plant!
+          </Typography>
+        </Box>
+        <Box mt={-15} mb={8} className='index'>
+          <TFYPAboutSection></TFYPAboutSection>
+        </Box>
+      </Container>
+      <Container maxWidth='lg'>
         <div
           style={{
             width: '100px',
@@ -145,7 +118,7 @@ const IndexPage = ({ treeBenefits }: { treeBenefits: TitleSection[] }) => {
           }}
         ></div>
         <Typography variant='h2' color='secondary' mb={6}>
-          Why Thank-a-Tree?
+          Why Support the Urban Forest with an Annual Membership?
         </Typography>
         <Grid container direction={{ xs: 'column', sm: 'row', md: 'row' }} spacing={2} mb={5}>
           {treeBenefits.map((benefit, idx) => (
@@ -165,75 +138,52 @@ const IndexPage = ({ treeBenefits }: { treeBenefits: TitleSection[] }) => {
           ))}
         </Grid>
       </Container>
-      <div style={{ height: '150px', width: '100%', marginTop: '-40px', overflow: 'hidden' }}>
+      <div style={{ height: '150px', width: '100%', marginTop: '-80px', overflow: 'hidden' }}>
         <CurveTop />
       </div>
       <div className='wide-container index'>
-        <Container maxWidth='lg'>
-          <Typography variant='h2' color='white' mb={4} mt={4} sx={{ textAlign: 'center' }}>
-            How can I Thank-a-Tree?
+        <Container maxWidth='sm'>
+          <Typography color='white' variant='h1' sx={{ fontSize: '2rem' }}>
+            Become a Member
           </Typography>
-          <Grid container direction={{ xs: 'column', sm: 'row' }}>
-            <Grid item sm={4}>
-              <Box>
-                <Box className='step-number'>1</Box>
-              </Box>
-              <Typography variant='subtitle1' className='step-text'>
-                Snap a picture of your most appreciated tree(s)
-              </Typography>
-            </Grid>
-            <Grid item sm={4}>
-              <Box>
-                <Box className='step-number'>2</Box>
-              </Box>
-              <Typography variant='subtitle1' className='step-text'>
-                Support your tree(s) and future tree plantings with an annual TreeFolks donation of $20/tree
-              </Typography>
-            </Grid>
-            <Grid item sm={4} className='center'>
-              <Box>
-                <Box className='step-number'>3</Box>
-              </Box>
-              <Typography variant='subtitle1' className='step-text'>
-                Thank-a-Tree by adding a Token of Appre-tree-ation to the map
-              </Typography>
-              <Link href='/signup'>
-                <Button variant='outlined' color='inherit'>
-                  Get Started
-                </Button>
-              </Link>
-            </Grid>
-          </Grid>
+          <Typography variant='body2' mb={-1}>
+            Support the Central Texas urban forest by becoming a TreeFolks Young Professionals (TreeFolksYP) member. Please select an annual
+            TreeFolks donation level below:
+          </Typography>
         </Container>
       </div>
-      <div style={{ height: '150px', width: '100%', overflow: 'hidden', transform: 'scaleY(-1) scaleX(-1)' }}>
+      <div
+        style={{
+          height: '360px',
+          marginBottom: '-360px',
+          width: '100%',
+          overflow: 'hidden',
+          transform: 'scaleY(-1) scaleX(-1)',
+          marginTop: '-1px',
+        }}
+      >
         <CurveBottom />
       </div>
-      <Container maxWidth='lg'>
-        <Grid mt={0} className={classes.treeDetailsContainer} alignItems='center'>
-          <Typography variant='h2' color='secondary'>
-            Support the Urban Forest
-          </Typography>
-          <Typography variant='subtitle1' className={classes.headlineSubTitle}>
-            Your donation is a 100% tax deductible donation to TreeFolks to plant, care for, and give away trees in the Austin and Central
-            Texas Community. Checkout a few Tokens of Appre-tree-ation:
-          </Typography>
-
-          <SponsorshipGroup sponsorships={sponsorships} isLoading={isLoadingSponsorships} />
-        </Grid>
-        <Box mt={8} p={5} className='index detail-section'>
-          <Typography variant='h2'>What is TreeFolks Young Professionals?</Typography>
-          <Typography variant='subtitle1' className={classes.headlineSubTitle}>
-            TreeFolks Young Professionals (ages 21 – 40ish) volunteer, educate, fundraise, and build community in support of the mission of
-            TreeFolks: planting, caring for, and giving people free trees to plant!
-          </Typography>
-        </Box>
-        <Box mt={-15} className='index'>
-          <TFYPAboutSection></TFYPAboutSection>
-        </Box>
-      </Container>
+      <Box>
+        <SignupForm
+          stripePriceIdHigh={stripePriceIdHigh}
+          stripePriceIdLow={stripePriceIdLow}
+          stripePriceIdMedium={stripePriceIdMedium}
+        ></SignupForm>
+      </Box>
     </Layout>
   );
 };
 
 export default IndexPage;
+
+export async function getServerSideProps() {
+  return {
+    props: {
+      stripePriceIdLow: process.env.STRIPE_PRICE_ID_LOW,
+      stripePriceIdMedium: process.env.STRIPE_PRICE_ID_MEDIUM,
+      stripePriceIdHigh: process.env.STRIPE_PRICE_ID_HIGH,
+      treeBenefits: treeBenefits,
+    },
+  };
+}
