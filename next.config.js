7c968af74df99de1012aca13fefed95c32780343
Kyle Hoskins <kyle2829@gmail.com>
2023-01-21 19:58:08 -0600

    fix: allow treefolks.org images

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /* eslint-disable no-undef */
 module.exports = {
   images: {
-    domains: [`${process.env.AWS_BUCKET_NAME}.s3.amazonaws.com`, 'platform-lookaside.fbsbx.com', 'lh3.googleusercontent.com'],
+    domains: [
+      `${process.env.AWS_BUCKET_NAME}.s3.amazonaws.com`,
+      'platform-lookaside.fbsbx.com',
+      'lh3.googleusercontent.com',
+      'treefolks.org',
+    ],
   },
   swcMinify: false,
 };
