98d8128451f89a9bc4db611fc25c835a401f00c8
Kyle Hoskins <kyle2829@gmail.com>
2023-04-17 19:41:21 -0500

    feat: update species search priority

---
+++ utils/tree/update-species-priority.ts
@@ -0,0 +1,9 @@
+export async function updateSpeciesPriority() {
+  await prisma.$executeRaw`UPDATE Species s
+
+    LEFT JOIN (SELECT count(*) as priority, speciesId from Tree where speciesId IS NOT NULL GROUP BY speciesId) as s2 ON
+        s.id=s2.speciesId
+    SET
+        s.searchPriority  = s2.priority + s.isInTexas
+    WHERE s2.priority IS NOT NULL`;
+}
