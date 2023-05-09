1da4d003e4636b50cceef15209c1b226f00fef64
Kyle Hoskins <kyle2829@gmail.com>
2023-05-09 14:20:02 -0500

    remove extra code

--- pages/api/events/[id]/trees.ts
+++ pages/api/events/[id]/trees.ts
@@ -1,17 +1,9 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import throwError from 'utils/api/throw-error';
 import { prisma, Prisma } from 'utils/prisma/init';
-import { isCurrentUserAuthorized } from 'utils/auth/is-current-user-authorized';
 import { PartialTree } from 'interfaces';
-import { v4 as uuidv4 } from 'uuid';
-
-import uploadTreeImages from 'utils/aws/upload-tree-images';
-import getTreeImagePath from 'utils/aws/get-tree-image-path';
-import { Tree } from '@prisma/client';
 import { getLocationFilterByDistance } from 'utils/prisma/get-location-filter-by-distance';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  //const id = Number(req.query.id);
   if (req.method === 'GET') {
     const eventId = Number(req.query.id);
     const email = req.query.email + '';
@@ -38,19 +30,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         include: includeFilter,
       })) as PartialTree[];
     }
-    /*
-    const result = await prisma.event.findFirst({
-      where: { id: id },
-      include: {
-        location: {
-          include: {
-            trees: {
-              include: { sponsorships: {}, species: {} },
-            },
-          },
-        },
-      },
-    });*/
     res.status(200).json(trees);
   }
 }
