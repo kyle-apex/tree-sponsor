7121adf816a3ad59c55e1bc47b4594ac1846a82b
Kyle Hoskins <kyle2829@gmail.com>
2023-06-28 22:16:51 -0500

    progress: TreeID Leader Position

--- components/event/TreeIdLeaderPosition.tsx
+++ components/event/TreeIdLeaderPosition.tsx
@@ -2,6 +2,9 @@ import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import UserAvatar from 'components/sponsor/UserAvatar';
+import LeaderboardIcon from '@mui/icons-material/Leaderboard';
+import Link from 'next/link';
+
 import { PartialUser } from 'interfaces';
 type U = PartialUser & { count: number };
 const TreeIdLeaderPosition = () => {
@@ -11,20 +14,84 @@ const TreeIdLeaderPosition = () => {
     { name: 'Sean', count: 2 },
   ];
   return (
-    <Box>
+    <Box
+      sx={{
+        background: 'linear-gradient(to top, #486e624f, #486e6233), url(/background-lighter.svg)',
+        pl: 1,
+        height: '118px',
+        position: 'relative',
+        borderRadius: '5px',
+        mt: 2,
+        mb: 2,
+      }}
+      className='box-shadow'
+    >
+      <Box sx={{ position: 'absolute', left: '5px', top: '1px', fontSize: '80%' }}>
+        <Link href='/leaders'>
+          <a
+            style={{
+              textDecoration: 'underline',
+              cursor: 'pointer',
+              display: 'flex',
+              gap: '3px',
+              alignItems: 'center',
+              justifyContent: 'end',
+            }}
+          >
+            <LeaderboardIcon sx={{ fontSize: 'inherit' }}></LeaderboardIcon> View full leaderboard
+          </a>
+        </Link>
+      </Box>
+      <Box sx={{ position: 'absolute', right: '5px', top: '1px', fontSize: '80%' }}>Correct Guesses</Box>
       {leaders.map((user, idx) => {
         return (
-          <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', mt: -1.5 }} key={user.name}>
-            <Typography sx={{ mr: 1 }}>{idx + 1}.</Typography>
-            <Box sx={{ ml: idx == 1 ? 1 : 0 }}>
-              <UserAvatar image={user.image} name={user.displayName || user.name} size={30} />
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'row',
+              alignItems: 'center',
+              position: 'absolute',
+              top: 24 + idx * 28 + 'px',
+              zIndex: idx == 1 ? 23 : 1,
+              fontWeight: idx == 1 ? 600 : 400,
+              width: '100%',
+              pr: 3,
+            }}
+            key={user.name}
+          >
+            <Typography sx={{ fontWeight: 600, flex: '1 0 34px', textAlign: 'center', marginRight: 0.5 }}>{idx + 1}</Typography>
+            <Box sx={{ ml: idx == 1 ? '-2px' : 0, mt: idx == 1 ? '-2px' : 0 }}>
+              <UserAvatar
+                sx={{ border: idx == 1 ? 'solid 1px white' : null }}
+                image={user.image}
+                name={user.displayName || user.name}
+                size={idx == 1 ? 34 : 30}
+              />
             </Box>
 
-            <Typography variant='subtitle2' sx={{ marginLeft: 1 }}>
+            <Typography variant='subtitle2' sx={{ marginLeft: idx == 1 ? 1 : '10px' }}>
               {user.name}
             </Typography>
             <Typography variant='subtitle2' color='GrayText' sx={{ fontSize: '.8rem', marginLeft: 1 }}>
               Member
+            </Typography>
+            <Box sx={{ flex: '1 1 100%' }}></Box>
+            <Typography
+              variant='subtitle2'
+              sx={{
+                textAlign: 'center',
+                /*backgroundColor: '#486E62',*/
+                background: 'linear-gradient(to top, #486e62, #486e62cc),url(/background-lighter.svg)',
+                color: 'white',
+                padding: '2px 10px',
+                borderRadius: '20px',
+                flex: '1 0 44px',
+                fontWeight: 'bold',
+                fontSize: '.75rem',
+              }}
+              className='box-shadow'
+            >
+              {user.count}
             </Typography>
           </Box>
         );
