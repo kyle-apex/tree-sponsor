7aa5401a30e59ead33f444822df78dc3225ae1ef
Kyle Hoskins <kyle2829@gmail.com>
2024-07-14 12:30:49 -0500

    remove logs

--- server-prod.js
+++ server-prod.js
@@ -5,23 +5,22 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 const dev = process.env.NODE_ENV !== 'production';
-console.log('dev', dev);
 const hostname = dev ? 'localhost' : '0.0.0.0';
 const port = dev ? 3443 : process.env.PORT;
+
 // when using middleware `hostname` and `port` must be provided below
 const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
+
 app.prepare().then(() => {
   createServer(async (req, res) => {
     const host = req.headers.host;
     const subdomain = host.split('.')[0]; // Extract subdomain
-    console.log('subdomain', subdomain);
     if (subdomain && subdomain != 'www' && subdomain != 'tfyp' && subdomain != 'treefolksyp-dev') {
       // Redirect to another page or route
       let subdomainRedirect;
       try {
         subdomainRedirect = await prisma.subdomainRedirect.findFirst({ where: { subdomain } });
-        console.log('subdomainRedirect', subdomainRedirect);
       } catch (err) {}
       if (subdomainRedirect?.redirect) {
         res.writeHead(302, { location: subdomainRedirect.redirect });
