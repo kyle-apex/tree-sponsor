5fcca518119cb1da12ec05f8f68e8d67cf3b1e52
Kyle Hoskins <kyle2829@gmail.com>
2024-07-14 12:07:47 -0500

    testing subdomain redirect

--- server-prod.js
+++ server-prod.js
@@ -1,9 +1,9 @@
-const { createServer } = require('http')
-const next = require('next')
+const { createServer } = require('http');
+const next = require('next');
 
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
- 
+
 const dev = process.env.NODE_ENV !== 'production';
 console.log('dev', dev);
 const hostname = dev ? 'localhost' : '0.0.0.0';
@@ -14,28 +14,29 @@ const handle = app.getRequestHandler();
 app.prepare().then(() => {
   createServer(async (req, res) => {
     const host = req.headers.host;
-        const subdomain = host.split('.')[0]; // Extract subdomain
-        console.log('subdomain', subdomain);
-        if (subdomain && subdomain != 'www' && subdomain != 'tfyp') {
-          // Redirect to another page or route
-          let subdomainRedirect;
-          try {
-            subdomainRedirect = await prisma.subdomainRedirect.findFirst({ where: { subdomain } });
-            console.log('subdomainRedirect', subdomainRedirect);
-    
-          } catch (err) {}
-          if (subdomainRedirect?.redirect) res.redirect(subdomainRedirect.redirect);
-          else await handle(req, res)
-        } else {
-            await handle(req, res)
-        }
-    
-  }) 
-    .once('error', (err) => {
-      console.error('got an error', err)
-      process.exit(1)
+    const subdomain = host.split('.')[0]; // Extract subdomain
+    console.log('subdomain', subdomain);
+    if (subdomain && subdomain != 'www' && subdomain != 'tfyp' && subdomain != 'treefolksyp-dev') {
+      // Redirect to another page or route
+      let subdomainRedirect;
+      try {
+        subdomainRedirect = await prisma.subdomainRedirect.findFirst({ where: { subdomain } });
+        console.log('subdomainRedirect', subdomainRedirect);
+      } catch (err) {}
+      if (subdomainRedirect?.redirect) {
+        res.redirect();
+        res.writeHead(302, { location: subdomainRedirect.redirect });
+        res.end();
+      } else await handle(req, res);
+    } else {
+      await handle(req, res);
+    }
+  })
+    .once('error', err => {
+      console.error('got an error', err);
+      process.exit(1);
     })
     .listen(port, () => {
-      console.log(`> Ready on http://${hostname}:${port}`)
-    })
-})
+      console.log(`> Ready on http://${hostname}:${port}`);
+    });
+});
