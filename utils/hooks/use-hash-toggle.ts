d5bd34ebd1bf8a9ea7730b22030407fedd5ffa69
Kyle Hoskins <kyle2829@gmail.com>
2024-08-18 12:26:42 -0500

    fix: initial value from path in use had toggle

--- utils/hooks/use-hash-toggle.ts
+++ utils/hooks/use-hash-toggle.ts
@@ -3,16 +3,16 @@ import { useState, useEffect, Dispatch, SetStateAction } from 'react';
 
 export default function useHashToggle(hash: string, defaultValue?: boolean): [boolean, (value: boolean) => void] {
   const initial = !!defaultValue;
-  const [value, setValue] = useState(initial || false);
   const router = useRouter();
+  const initialValueFromRoute = router.asPath?.includes('#') && router.asPath.split('#')[1] == hash;
+
+  const [value, setValue] = useState(initialValueFromRoute || initial || false);
+
   useEffect(() => {
     if (!router.asPath?.includes('#')) return;
     if (router.asPath.split('#')[1] == hash) setValue(true);
   }, []);
   const setHashValue = (value: boolean) => {
-    //console.log('pathname', router.pathname);
-    //console.log('path', router.asPath);
-    //console.log('router', router);
     const path = router.asPath ? router.asPath.split('#')[0] : '';
     if (value) router.push(path, `#${hash}`);
     else router.push(path);
