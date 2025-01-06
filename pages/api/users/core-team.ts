6d5bce74b7f1a393a90967dfe74737fc414ba5f5
Kyle Hoskins <kyle2829@gmail.com>
2025-01-05 19:06:40 -0600

    Remove cors from core-team endpoint

--- pages/api/users/core-team.ts
+++ pages/api/users/core-team.ts
@@ -1,7 +1,13 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getCoreTeamBios } from 'utils/user/get-core-team-bios';
+import Cors from 'micro-cors';
 
-export default async function handler(_req: NextApiRequest, res: NextApiResponse) {
+const cors = Cors({
+  allowMethods: ['GET'],
+});
+const getBios = async function handler(_req: NextApiRequest, res: NextApiResponse) {
   const users = await getCoreTeamBios();
   res.status(200).json(users);
-}
+};
+
+export default cors(getBios as any);
