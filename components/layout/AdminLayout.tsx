b967321bc4254d319d2c02c8fdee0d5a2874ce20
Kyle Hoskins <kyle2829@gmail.com>
2025-01-07 21:05:00 -0600

    feat: admin navigation menu

--- components/layout/AdminLayout.tsx
+++ components/layout/AdminLayout.tsx
@@ -2,13 +2,19 @@ import Button from '@mui/material/Button';
 import Link from 'next/link';
 import React, { ReactNode } from 'react';
 import Layout from './Layout';
+import SplitRow from './SplitRow';
+import NavigationMenu from 'components/admin/NavigationMenu';
 
 const AdminLayout = ({ children, title, header }: { children?: ReactNode; title?: string; header?: string | ReactNode }) => {
   return (
     <Layout title={title}>
-      <Link href='/admin/'>
-        <Button variant='outlined'>Back to Admin Home</Button>
-      </Link>
+      <SplitRow>
+        <Link href='/admin/'>
+          <Button variant='outlined'>Back to Admin Home</Button>
+        </Link>
+        <NavigationMenu></NavigationMenu>
+      </SplitRow>
+
       <h1>{header}</h1>
       {children}
     </Layout>
