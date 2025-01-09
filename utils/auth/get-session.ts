fc7552ef408bb467e97523e6f04e002b002661b2
Kyle Hoskins <kyle2829@gmail.com>
2025-01-08 20:01:41 -0600

    fix: remove excess log

--- utils/auth/get-session.ts
+++ utils/auth/get-session.ts
@@ -2,7 +2,6 @@ import { getSession as getNextSession, GetSessionOptions } from 'next-auth/clien
 import { Session, NextSession } from 'interfaces';
 export async function getSession(options?: GetSessionOptions): Promise<Session | null> {
   return getNextSession(options).then((nextSession: NextSession) => {
-    console.log('nextSession', nextSession);
     return nextSession as Session;
   });
 }
